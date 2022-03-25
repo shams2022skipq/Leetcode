b9b4bdeb6a073765d60c59451ac9c88ccb6bc19d
shams <shams.orakzai.777@gmail.com>
2022-03-25 16:41:47 +0500

    Plus one problem solved.

---
+++ 4. Plus One Problem.py
@@ -0,0 +1,14 @@
+def plusOne(digits):
+    if digits[-1] < 9:
+        digits[-1] += 1
+        return digits
+    elif len(digits) == 1 and digits[0] == 9:
+        return [1, 0]
+    else:
+        digits[-1] = 0
+        digits[0:-1] = plusOne(digits[0:-1])
+        return digits
+
+
+print(plusOne([1, 2, 4]))
+
